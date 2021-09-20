c599ecb7d07d198574c40d0e27143ac8b2736cb0
mohammedgafer75 <mohammedgafer75@gmail.com>
2021-09-20 20:02:19 +0200

    make it better

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports ={
-    MONGODB: 'mongodb+srv://mohammedgafer75:moh09122@cluster0.vvdx5.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+    MONGODB: 'mongodb+srv://moh09122:moh09122@cluster0.6u7q0.mongodb.net/faelkhair?retryWrites=true&w=majority',
     //mongodb://localhost:27017/Flutter_overFlow
     //mongodb+srv://mohammedgafer75:moh09122@cluster0.vvdx5.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
     // 'mongodb+srv://moh09122:moh09122@cluster0.6u7q0.mongodb.net/faelkhair?retryWrites=true&w=majority' || 
